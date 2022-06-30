06ca29569b1c19c5b1e353f21d00f81882b954f8
Manthan Chauhan <manthan321.mc@gmail.com>
2022-06-30 16:57:56 +0530

    error fix 2

--- application/views/admin/common/footer_view.php
+++ application/views/admin/common/footer_view.php
@@ -37,8 +37,8 @@
 
     </script>
 
-<script>
-<script type="text/javascript" src="<?php echo base_url() ?>assets/slider/ajaxupload.3.5.js"></script>
+    <script type="text/javascript" src="<?php echo base_url() ?>assets/slider/ajaxupload.3.5.js"></script>
+
           <script>
           // Attach a submit handler to the form
           $(document).ready(function(){
